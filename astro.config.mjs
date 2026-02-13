0a82af79d418b3054ddc9f33d2e3cee611ec6edb
npremont <ni.premont@gmail.com>
2026-02-13 11:00:27 +0100

    v0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://consciences.be',
   integrations: [react()],
 
   vite: {
