0cf02f7e66094f7e50e259299a87407673b058a2
npremont <ni.premont@gmail.com>
2026-02-13 11:07:57 +0100

    Add Node.js adapter for SSR deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://consciences.be',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react()],
 
   vite: {
